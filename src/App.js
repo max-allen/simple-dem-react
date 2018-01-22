07b256c032d2aced955b2f6b7720f0d80851adff
max-allen <maxton.allen@gmail.com>
2018-01-21 21:35:05 -0500

    boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	entry: [
+		'./src/App.js'
+
+	],
+	output: {
+		path: __dirname,
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			options: {
+				presets: ['react']
+			}
+		}]
+	}
+};
